dae5d7af1fc5fbe6141d06ae32b41df4b5a23988
Junyoung Choi <fluke8259@gmail.com>
2025-11-03 15:14:40 +0900

    Remove original code

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-export * from './types'
-export * from './errors'
-export { KenmonAuthService } from './auth'
-export {
-  EmailOTPAuthProvider,
-  KenmonOTPError,
-  type KenmonOTPStorage,
-  type KenmonOTPErrorReason,
-} from './providers/emailOTP'
--- src/nextjs/index.ts
+++
@@ -1,1 +0,0 @@
-export { KenmonNextJSAdapter } from './adapter'
